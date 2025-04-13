18227380366cae8cdcc3136f5d21699db6d66857
raviendalpatadu <102800797+raviendalpatadu@users.noreply.github.com>
2025-04-13 13:36:00 +0530

    Refactor Tailwind configuration, enhance Vite server settings, and improve App component structure. Added font-face for 'Revalia' and implemented a background image carousel in Home component. Updated Navbar and CSS for better styling and layout adjustments.

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Home from './pages/Home';
-import About from './pages/About';
-import Achievements from './pages/Achievements1';
-import Gallery from './pages/Gallery';
-import Footer from './components/Footer';
-import Parallax from './components/Parallax';
-import HomeSecond from './pages/HomeSecond';
-import NewGallery from './pages/NewGallery';
-import Achievement from './components/achievements/Achievement';
-import Contact from './components/contact/Contact';
-
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Achievements from "./pages/Achievements1";
+import Gallery from "./pages/Gallery";
+import Footer from "./components/Footer";
+import Parallax from "./components/Parallax";
+import HomeSecond from "./pages/HomeSecond";
+import NewGallery from "./pages/NewGallery";
+import Achievement from "./components/achievements/Achievement";
+import Contact from "./components/contact/Contact";
 
 function App() {
   return (
@@ -19,35 +17,53 @@ function App() {
       <div className="min-h-screen flex flex-col">
         <Navbar />
         <main className="flex-grow">
-        <Routes>
-        <Route path="/" element={
-          <>
-        <section id="Home" className="relative h-[100vh] md:h-screen snap-start md:snap-center">
-        <Home/>
-      </section>
-      <section className="relative h-[180vh] md:h-screen snap-start md:snap-center">
-        <HomeSecond/>
-      </section>
-        <section className="relative h-[100vh] md:h-screen snap-start md:snap-center">
-        <Parallax type="About Us"/>
-      </section>
-      <section id="About" className="px-4 relative h-[180vh] md:h-screen snap-start md:snap-center">
-        <About/>  
-      </section>
-      <section  className="py-4 relative h-[100vh] md:h-screen snap-start md:snap-center" id="Achievements">
-        <Parallax type="Achievements"/>
-      </section>
-      <Achievement />
-      <section id="Gallery" className="px-4 relative h-[60vh] md:h-screen md:snap-center">
-        <Gallery/>  
-      </section>
-      <section id="Contact" className="px-4 relative h-[140vh] snap-start md:h-screen md:snap-center">
-        <Contact/>
-      </section>
-      </>
-      }/>
-      <Route path="/newgallery" element={<NewGallery/>}/>
-      </Routes>
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <>
+                  <section
+                    id="Home"
+                    className="relative h-[100vh] md:h-screen snap-start md:snap-center"
+                  >
+                    <Home />
+                  </section>
+                  <section className="relative h-[180vh] md:h-screen snap-start md:snap-center">
+                    <HomeSecond />
+                  </section>
+                  <section className="relative h-[100vh] md:h-screen snap-start md:snap-center">
+                    <Parallax type="About Us" />
+                  </section>
+                  <section
+                    id="About"
+                    className="px-4 relative h-[180vh] md:h-screen snap-start md:snap-center"
+                  >
+                    <About />
+                  </section>
+                  <section
+                    className="py-4 relative h-[100vh] md:h-screen snap-start md:snap-center"
+                    id="Achievements"
+                  >
+                    <Parallax type="Achievements" />
+                  </section>
+                  <Achievement />
+                  <section
+                    id="Gallery"
+                    className="px-4 relative h-[60vh] md:h-screen md:snap-center"
+                  >
+                    <Gallery />
+                  </section>
+                  <section
+                    id="Contact"
+                    className="px-4 relative h-[140vh] snap-start md:h-screen md:snap-center"
+                  >
+                    <Contact />
+                  </section>
+                </>
+              }
+            />
+            <Route path="/newgallery" element={<NewGallery />} />
+          </Routes>
         </main>
         <Footer />
       </div>
