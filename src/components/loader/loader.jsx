2b72f404be0ea66b824825ba6945c7603aa4b7e5
raviendalpatadu <102800797+raviendalpatadu@users.noreply.github.com>
2025-04-14 15:53:56 +0530

    Replace loading spinner with video animation in Loader component for enhanced visual experience. Update styles for better layout and responsiveness.

--- src/components/loader/loader.jsx
+++ src/components/loader/loader.jsx
@@ -1,9 +1,18 @@
-import "./loader.scss"
+import loaderVideo from "../../images/Animation - 1744624841261.webm"
 
 const Loader = () => {
-
   return (
-    <span class="loader"></span>
+    <div className="w-40 h-40 relative top-12">
+      <video
+        autoPlay
+        loop
+        muted
+        playsInline
+        className="inset-0 w-full h-full object-cover"
+      >
+        <source src={loaderVideo} type="video/webm" />
+      </video>
+    </div>
   )
 }
 
